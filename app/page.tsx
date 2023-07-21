625ca5c39361f92cdc2f5082183b7b6e90244c93
Zac <yihongle@buffalo.edu>
2023-07-20 21:06:42 -0400

    get org data

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,20 @@
-import { auth } from "@clerk/nextjs";
+import { UserProfile, auth } from "@clerk/nextjs";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import { SignOutButton } from "@clerk/nextjs";
 
 export default function Home() {
-    const { userId } = auth();
+    const { userId, orgId, orgRole, orgSlug } = auth();
     if (!userId) {
         redirect("/sign-in");
     }
     return (
         <div className="flex flex-col">
+            <div>In org: {orgId} {orgRole} {orgSlug}</div>
             <Link href="/create-org">Create an org</Link>
             <Link href="/view-org">View my org</Link>
             <Link href="/switch-orgs">Switch org</Link>
+            <UserProfile />
             <SignOutButton />
         </div>
     );
