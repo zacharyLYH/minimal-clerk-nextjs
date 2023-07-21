3b7e4b2a45dceda3418890bb9ed49e5aed83eef1
Zac <yihongle@buffalo.edu>
2023-07-20 21:06:52 -0400

    get user data from client component

--- app/switch-orgs/page.tsx
+++ app/switch-orgs/page.tsx
@@ -1,12 +1,13 @@
 "use client"
 
-import { OrganizationSwitcher } from '@clerk/nextjs'
+import { OrganizationSwitcher, useUser } from '@clerk/nextjs'
 import Link from 'next/link'
 
 export default function HomePage() {
+  const { user } = useUser()
   return (
     <div>
-        <Link href="/">Dashboard</Link>
+        <Link href="/">Dashboard for {user?.fullName}</Link>
         <OrganizationSwitcher />
     </div>
   )
