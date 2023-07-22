c2e78b8fc66b76404249ab0eba79e12cc42d1df4
Zac <yihongle@buffalo.edu>
2023-07-22 11:51:02 -0400

    confirmed that useAuth() doesn't poll server. will need to do server side auth before doing sensitive actions

--- app/get-facts/page.tsx
+++ app/get-facts/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useAuth } from "@clerk/nextjs";
 import { OrganizationMembership } from "@clerk/nextjs/dist/types/server/clerkClient";
 import Link from "next/link";
 import { useState } from "react";
@@ -7,20 +8,25 @@ import { useState } from "react";
 const ShowFacts = () => {
     const [error, setError] = useState("");
     const [facts, setFacts] = useState<OrganizationMembership[]>([]);
+    const { orgId } = useAuth();
     const fetcher = async () => {
+        if(orgId === null){
+            return new Error("You don't belong to this ORG!")
+        }
         try {
             const data = await fetch("/api/fakeReq");
             setFacts(await data.json());
-            setError("")
-        }catch(error){
-            console.log("ERROR FOUND")
-            setError("Error found")
-            setFacts([])
+            setError("");
+        } catch (error) {
+            console.log("ERROR FOUND");
+            setError("Error found");
+            setFacts([]);
         }
     };
     return (
         <div className="flex flex-col">
             <Link href="/">Dashboard</Link>
+            {/* {orgId !== null && (<p>This org is: {orgId}</p>)} */}
             <button onClick={fetcher}>Show facts</button>
             {facts.length > 0 &&
                 facts.map((fact) => (
