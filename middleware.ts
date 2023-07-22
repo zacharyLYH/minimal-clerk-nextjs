e7ea736fb6449219aab194044ffb6996949f8784
Zac <yihongle@buffalo.edu>
2023-07-22 10:19:31 -0400

    use sophisticated clerk mw

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,23 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, clerkClient } from "@clerk/nextjs";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
-export default authMiddleware({});
+export default authMiddleware({
+    afterAuth(auth, req, evt) {
+        // console.log(auth.orgId, auth.orgRole)
+        const verifyUser = async () => {
+            if (auth.orgId) {
+                const memberships =
+                    await clerkClient.organizations.getOrganizationMembershipList(
+                        { organizationId: auth.orgId }
+                    );
+                console.log(memberships[0].publicUserData?.userId);
+            }
+        };
+        verifyUser();
+    },
+});
 
 export const config = {
     matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
