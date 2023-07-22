9b029109296d07d2db654fea6653fb765b59c919
Zac <yihongle@buffalo.edu>
2023-07-22 11:50:15 -0400

    confirmed that auth() polls clerk each time

--- app/api/fakeReq/route.ts
+++ app/api/fakeReq/route.ts
@@ -1,26 +1,24 @@
 import { auth, clerkClient } from "@clerk/nextjs";
-import { headers } from "next/dist/client/components/headers";
 import { NextResponse } from "next/server";
 
 export const GET = async () => {
     const { userId, orgId } = auth();
-    const ret = { userId, orgId };
     if (orgId) {
         const memberships =
             await clerkClient.organizations.getOrganizationMembershipList({
                 organizationId: orgId,
             });
-        let authorized = false;
-        for (const mem of memberships) {
-            if (mem.publicUserData?.userId === userId) {
-                authorized = true;
-                break;
-            }
-        }
-        if (!authorized) {
-            console.log("UNAUTHORIZED_FAKEREQ");
-            return new NextResponse("Unauthorized", { status: 401 });
-        }
+        // let authorized = false;
+        // for (const mem of memberships) {
+        //     if (mem.publicUserData?.userId === userId) {
+        //         authorized = true;
+        //         break;
+        //     }
+        // }
+        // if (!authorized) {
+        //     console.log("UNAUTHORIZED_FAKEREQ");
+        //     return new NextResponse("Unauthorized", { status: 401 });
+        // }
         return NextResponse.json(memberships);
     } else {
         return new NextResponse("No org ID", { status: 404 });
